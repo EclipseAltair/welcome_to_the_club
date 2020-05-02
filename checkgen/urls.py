213e8c0938eb2b209a64e05e9d0a8522fb91d93d
EclipseAltair <eclipsealtair@yandex.ru>
2020-05-02 21:28:48 +0300

    create template views.py

--- checkgen/urls.py
+++ checkgen/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^create_checks/$', views.create),
+    url(r'^create_checks/$', views.create_checks),
+    url(r'^new_checks/$', views.new_checks),
+    url(r'^check/$', views.check),
 ]
--- checkgen/views.py
+++ checkgen/views.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 
-def create(request):
-    return JsonResponse({})
+def create_checks(request):
+    if request.POST:
+        return HttpResponse({})
+
+
+def new_checks(request):
+    return HttpResponse({})
+
+
+def check(request):
+    return HttpResponse({})
