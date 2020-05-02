e4e585f46caabd0d26c02e3889671de49a48d4b7
EclipseAltair <eclipsealtair@yandex.ru>
2020-05-02 16:00:48 +0300

    create admin.py

--- checkgen/admin.py
+++ checkgen/admin.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from .models import Printer, Check
+
+
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Printer._meta.fields]
+
+
+class CheckAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Check._meta.fields]
+    list_filter = ["printer_id", "type", "status"]
+
+
+admin.site.register(Printer, PrinterAdmin)
+admin.site.register(Check, CheckAdmin)
