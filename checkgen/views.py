1e8d52da5d6b35a5c390f242b7093b907aed2dcd
EclipseAltair <eclipsealtair@yandex.ru>
2020-05-04 17:37:29 +0300

    restructure views.py

--- checkgen/views.py
+++ checkgen/views.py
@@ -10,25 +10,19 @@ from .wkhtmltopdf import generate_pdf
 
 
 @csrf_exempt    # исключение из CSRF-проверки для CORS
-def create_checks(request):
+def create_checks(request):    # создание чеков для заказа
     if request.method == 'POST':
         body_unicode = request.body.decode('UTF-8')
         try:
             body_data = json.loads(body_unicode)
         except json.JSONDecodeError:
-            response = JsonResponse({"error": "Invalid JSON"})
-            response.status_code = 400
-            return response
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
         
         if Check.objects.filter(order__id=body_data['id']).count() != 0:    # если заказ с таким id уже есть в БД
-            response = JsonResponse({"error": "Для данного заказа уже созданы чеки"})
-            response.status_code = 400
-            return response
+            return JsonResponse({"error": "Для данного заказа уже созданы чеки"}, status=400)
         
         if Printer.objects.filter(point_id=body_data['point_id']).count() == 0:    # если у точки нет принтера
-            response = JsonResponse({"error": "Для данной точки не настроено ни одного принтера"})
-            response.status_code = 400
-            return response
+            return JsonResponse({"error": "Для данной точки не настроено ни одного принтера"}, status=400)
 
         kitchen_printer = Printer.objects.filter(point_id=body_data['point_id'], check_type='kitchen')[0]
         kitchen_path = f'media/pdf/{body_data["id"]}_{"kitchen"}.pdf'
@@ -42,18 +36,18 @@ def create_checks(request):
                                             status='new', pdf_file=client_path)
         enqueue(generate_pdf, 'client', client_check.id, client_path, body_data)    # добавление в rq очередь
 
-        response = JsonResponse({"ok": "Чеки успешно созданы"})
-        response.status_code = 200
-        return response
-    
-    response = JsonResponse({"error": "Method Not Allowed"})
-    response.status_code = 405
-    return response
+        return JsonResponse({"ok": "Чеки успешно созданы"}, status=200)
+    return JsonResponse({"error": "Method Not Allowed"}, status=405)
         
 
-def new_checks(request):
-    return JsonResponse({})
+def new_checks(request):    # список доступных чеков для печати
+    if request.method == 'GET':
+        if Printer.objects:
+            return JsonResponse({"error": "Не существует принтера с таким api_key"}, status=401)
+        
+        return JsonResponse({"ok": "Чеки успешно созданы"}, status=200)
+    return JsonResponse({"error": "Method Not Allowed"}, status=405)
 
 
 def check(request):
-    return JsonResponse({})
+    return JsonResponse({"error": "Method Not Allowed"}, status=405)
